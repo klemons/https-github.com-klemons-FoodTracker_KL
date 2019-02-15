99ff21cb179e29e10270c4d4e28ff68cc402d594
Kyle Lemons <kal18c@lib-301-08-9116.local>
2019-02-15 08:28:11 -0600

    Initial Commit

---
+++ FoodTracker_KL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker_KL
+//
+//  Created by Kyle Lemons on 2/15/19.
+//  Copyright © 2019 Kyle Lemons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
